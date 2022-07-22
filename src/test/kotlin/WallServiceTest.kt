12ff08e6a430e3bb8543b87b04504f1613d08ef4
aleksandrgrigorev <xundr1992@gmail.com>
2022-07-22 13:09:08 +0400

    Tests for createComments added (Exceptions Task 2)

--- src/test/kotlin/WallServiceTest.kt
+++ src/test/kotlin/WallServiceTest.kt
@@ -46,4 +46,39 @@ class WallServiceTest {
         //assert
         assertFalse(result)
     }
+
+    @Test
+    fun createComment_shouldNotThrow() {
+        //arrange
+        val postId = 1
+        val comment = Comment()
+        for (i in range(0, 5)) {
+            WallService.add(Post(text = "TEST_MSG"))
+        }
+
+        //act
+        val result = WallService.createComment(
+            postId = postId,
+            comment = comment
+        )
+
+        //assert
+        assertEquals(comment, result)
+    }
+
+    @Test(expected = PostNotFoundException::class)
+    fun createComment_shouldThrow() {
+        //arrange
+        val postId = 0
+        val comment = Comment()
+        for (i in range(0, 5)) {
+            WallService.add(Post(text = "TEST_MSG"))
+        }
+
+        //act
+        WallService.createComment(
+            postId = postId,
+            comment = comment
+        )
+    }
 }
